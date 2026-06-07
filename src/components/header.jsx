1937753a827bb085284556520cddd1a9270a204e
dellod <daryl.dang4@gmail.com>
2026-06-07 18:36:45 -0400

    Add login and sign up buttons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 // React libraries
 import React from "react"
+import { Link } from "react-router-dom";
 
 // CSS
 import '../styles/header.css';
@@ -13,6 +14,14 @@ const Header = () =>
         <header className="header-stage">
             <img className="john-logo" src={Logo} alt="John logo" />
             <h1 className="header-title">Johnshin Impact</h1>
+            <div className="header-actions">
+                <Link to="/signup" className="action-link action-link-primary">
+                    Sign Up
+                </Link>
+                <Link to="/login" className="action-link action-link-secondary">
+                    Login
+                </Link>
+            </div>
         </header>
     );
 }
