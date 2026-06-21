2b3f6a38ca08beab07f58cda5c23db1bd3a25a2a
dellod <daryl.dang4@gmail.com>
2026-06-21 09:22:31 -0400

    change loading screen to link

--- src/app.js
+++ src/app.js
@@ -9,8 +9,9 @@ import './styles/app.css';
 import Header from './components/header';
 import Signup from './components/signup';
 import Login from './components/login';
-import LoadingScreenVideo from './assets/loading_screen.mp4';
 import LoadingScreenImage from './assets/loading_screen.jpg';
+
+// Cloudinary loading screen video URL - set via environment variable
 import Navbar from './components/navbar';
 import Achievements from './components/achievements';
 import Admin from './components/admin';
@@ -20,6 +21,8 @@ import Paimon from './components/paimon';
 
 // Scripts
 import { auth } from './scripts/firebase';
+
+const LOADING_SCREEN_VIDEO_URL = 'https://res.cloudinary.com/defgxcpnk/video/upload/v1782048059/loading_screen_hdhggj.mp4';
 
 const ProfileRoute = () => {
   const { slug } = useParams();
@@ -52,7 +55,7 @@ function App() {
           playsInline
           poster={LoadingScreenImage}
         >
-          <source src={LoadingScreenVideo} type="video/mp4" />
+          <source src={LOADING_SCREEN_VIDEO_URL} type="video/mp4" />
           <img src={LoadingScreenImage} alt="Loading background" />
         </video>
         <div className="bg-overlay" aria-hidden="true" />
@@ -70,7 +73,7 @@ function App() {
         playsInline
         poster={LoadingScreenImage}
       >
-        <source src={LoadingScreenVideo} type="video/mp4" />
+        <source src={LOADING_SCREEN_VIDEO_URL} type="video/mp4" />
         <img src={LoadingScreenImage} alt="Loading background" />
       </video>
 
